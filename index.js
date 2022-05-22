64a58cbd20fc04c3d815f9f0384bf7f63048638f
jamilagastar <jamilagastar@gmail.com>
2022-05-22 20:34:27 +1000

    added basic html and API with functions

---
+++ index.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+let kanyeQuoteButton = document.getElementById('kanyeQuoteGenerator');
+kanyeQuoteButton.addEventListener('click', fetchKanyeQuote);
+});
+
+function fetchKanyeQuote() {
+const url = "https://api.kanye.rest"
+    fetch(url)
+    .then((resp) => resp.json())
+    .then((data) => showKanyeQuote(data));
+}
+
+function showKanyeQuote(data) {
+let newQuote = document.createElement('p');
+newQuote.innerHTML = data.quote;
+
+document.querySelector("div").appendChild(newQuote);
+}
